c4cf2a388d2f35d131c2ddf8926e87c67b51a3cc
Lassi Kortela <lassi@lassi.io>
2016-12-19 01:11:27 +0200

    Move db into workdir and do git commits

--- git-sqlite-bench.py
+++ git-sqlite-bench.py
@@ -2,12 +2,15 @@
 
 import os
 import random
+import shutil
 import sqlite3
 from contextlib import contextmanager
 from subprocess import check_call, check_output
 
 
-DBFILE = os.path.join(os.path.dirname(__file__), 'bench.db')
+WORKDIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'work')
+DBFILE = os.path.join(WORKDIR, 'bench.db')
+DUMPFILE = os.path.join(WORKDIR, 'bench.sql')
 
 
 @contextmanager
@@ -15,6 +18,12 @@ def database():
     db = sqlite3.connect(DBFILE)
     yield db
     db.close()
+
+
+def dump_db_into_git(msg):
+    open(DUMPFILE, "wb").write(check_output(["sqlite3", DBFILE, ".dump"]))
+    check_call(["git", "add", DUMPFILE])
+    check_call(["git", "commit", "-m", msg])
 
 
 def randval():
@@ -46,17 +55,22 @@ def random_update(table, nfield, nrow):
 
 def bench(ntable, nfield, nop):
     nrow = {t: 0 for t in range(ntable)}
-    if os.path.isfile(DBFILE):
-        os.remove(DBFILE)
+    if os.path.isdir(WORKDIR):
+        shutil.rmtree(WORKDIR)
+    os.mkdir(WORKDIR)
+    os.chdir(WORKDIR)
+    check_call(["git", "init"])
     with database() as db:
         cur = db.cursor()
         for t in range(ntable):
             cur.execute(create_table(t, nfield))
+    dump_db_into_git("Commit empty database")
     for o in range(nop):
         t = random.randrange(ntable)
         op = random.choice(['insert', 'update']) \
              if nrow[t] else 'insert'
-        print("Operation #{}: {} table #{}".format(o, op, t))
+        opmsg = "Operation #{}: {} table #{}".format(o, op, t)
+        print(opmsg)
         with database() as db:
             cur = db.cursor()
             if op == 'insert':
@@ -65,6 +79,7 @@ def bench(ntable, nfield, nop):
             elif op == 'update':
                 cur.execute(random_update(t, nfield, nrow[t]))
             db.commit()
+        dump_db_into_git(opmsg)
 
 
 if __name__ == "__main__":
