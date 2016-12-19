208a47bbcd87f28e7c77ac3f1206f6d656cd374d
Lassi Kortela <lassi@lassi.io>
2016-12-19 03:38:40 +0200

    Log and plot runs
    Plot milliseconds per operation if GNU R is installed

---
+++ git-sqlite-bench.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -
+set -eu
+exec 2>&1
+cd "$(dirname "$0")"
+run="run-$(date '+%s')"
+time python3 -u git-sqlite-bench.py "$@" | tee "$run.log"
+! which Rscript || Rscript plot.R
+cp work/millisec "$run.millisec"
+for x in millisec*.png; do mv "$x" "$run.$x"; done
---
+++ plot.R
@@ -0,0 +1,7 @@
+png('millisec-hist.png')
+hist(scan('work/millisec'))
+dev.off()
+
+png('millisec-trend.png')
+plot(scan('work/millisec'))
+dev.off()
